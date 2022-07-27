54b84e23bcd48f5e89079c0c2913ebf2a56755cf
Anton Gusev <gans-AD@yandex.ru>
2022-07-27 23:29:13 +0300

    feat: схемы user и card

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
+// подключаем базу данных mestodb
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
 app.listen(PORT, () => {
-  console.log(`приложение запущено на порте ${PORT}`)
-})
+  console.log(`приложение запущено на порте ${PORT}`);
+});
