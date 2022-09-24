b8798207f726232cbaca194103c6697400629d1c
Anton Gusev <gans-AD@yandex.ru>
2022-09-24 22:35:05 +0300

    feat: валидация запроса с помощью celebrate

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.post('/signin', login);
 app.post(
   '/signup',
   celebrate({
-    body: Joi.object().keyes({
+    body: Joi.object().keys({
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
       avatar: Joi.string().uri(),
