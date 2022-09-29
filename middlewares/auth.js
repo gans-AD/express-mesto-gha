ae0a494809c4659f9f30692dafa365a3f3462c4a
Anton Gusev <gans-AD@yandex.ru>
2022-09-29 19:56:33 +0300

    fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -18,7 +18,7 @@ module.exports = (req, res, next) => {
   } catch {
     next(new AuthentificationError('Необходима авторизация'));
   }
-  
+
   req.user = payload; // записываем пейлоуд в объект запроса
   next();
 };
