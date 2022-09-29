df9912b30a6763910b10a4687b5266d17cb21c84
Anton Gusev <gans-AD@yandex.ru>
2022-09-29 13:26:32 +0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -30,15 +30,16 @@ const userSchema = new mongoose.Schema({
 userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email }).then((user) => {
     if (!user) {
-      throw new AuthentificationError('Неправильные почта или пароль1');
+      throw new AuthentificationError('Неправильные почта или пароль');
     }
-
     return bcrypt.compare(password, user.password).then((matched) => {
       if (!matched) {
         throw new AuthentificationError('Неправильные почта или пароль2');
-      }
+      } 
       return user;
-    });
+    }).catch((err) => {
+    res.status(600).send(user);
+    })
   });
 };
 
