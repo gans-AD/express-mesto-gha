21b44af7ddb09d4e1c8c2d34d5c35e8e1511e56e
Anton Gusev <gans-AD@yandex.ru>
2022-09-29 13:53:11 +0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -73,9 +73,7 @@ module.exports.login = (req, res, next) => {
           httpOnly: true,        
         }).send({data: user});
     })
-    .catch((err) => {
-      next(new AuthentificationError(`${err}`));
-  });
+    .catch(next);
 };
 
 // запрос всех пользователя
