abef3865d1dede825464d4410f2b1f19eaaad1c9
Anton Gusev <gans-AD@yandex.ru>
2022-09-29 11:33:46 +0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -70,7 +70,7 @@ module.exports.login = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,        
-        })
+        }).status(200)
         .send({data: user});
     })
     .catch(() => {
