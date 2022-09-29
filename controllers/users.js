3abba077d37bf01b32a2e0a7c7dfde3b5cc6b544
Anton Gusev <gans-AD@yandex.ru>
2022-09-29 12:32:03 +0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -59,6 +59,7 @@ module.exports.login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
+    console.log(user);
       // успешная аутентификация
       // создаем токен
       const token = jwt.sign({ _id: user._id }, 'some-secret-key', {
