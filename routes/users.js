516fc838c142f759b94961ddeae63ab54c5c184e
Anton Gusev <gans-AD@yandex.ru>
2022-09-30 01:00:29 +0300

    fix: валидация данных пользователя с  помощью Joi

--- routes/users.js
+++ routes/users.js
@@ -10,14 +10,13 @@ const {
 
 const validationUser = () => celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30).required(),
-    about: Joi.string().min(2).max(30).required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
     email: Joi.string().required().email(),
     link: Joi.string()
       .pattern(
         /^(https?:\/\/)?(w{3}\.)?([a-z0-9.-]+)\.([a-z.]{2,6})([a-zA-Z0-9-._~:/?#[]@!$&'()*\+,;=]*)*#?/,
-      )
-      .required(),
+      ),
   }),
 });
 
