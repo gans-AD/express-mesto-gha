b5ae29b3116bfae9a47b4a4b294e3faf8877f731
Anton Gusev <gans-AD@yandex.ru>
2022-09-30 00:16:20 +0300

    fix: валидация данных пользователя с  помощью Joi

--- app.js
+++ app.js
@@ -47,17 +47,33 @@ app.post(
 // мидлвэр авторизации
 app.use(auth);
 
-app.use('/users', require('./routes/users'));
+app.use(
+  '/users',
+  celebrate({
+    body: Joi.object.keys({
+      name: Joi.string().min(2).max(30).required(),
+      about: Joi.string().min(2).max(30).required(),
+      email: Joi.string().required().email(),
+      link: Joi.string()
+        .pattern(
+          /^(https?:\/\/)?(w{3}\.)?([a-z0-9.-]+)\.([a-z.]{2,6})([a-zA-Z0-9-._~:/?#[]@!$&'()*\+,;=]*)*#?/,
+        )
+        .required(),
+    }),
+  }),
+  require('./routes/users'),
+);
+
 app.use(
   '/cards',
   celebrate({
     body: Joi.object().keys({
       name: Joi.string().min(2).max(30).required(),
-      link: Joi
-        .string()
+      link: Joi.string()
         .pattern(
           /^(https?:\/\/)?(w{3}\.)?([a-z0-9.-]+)\.([a-z.]{2,6})([a-zA-Z0-9-._~:/?#[]@!$&'()*\+,;=]*)*#?/,
-        ).required(),
+        )
+        .required(),
     }),
   }),
   require('./routes/cards'),
