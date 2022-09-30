5bd48d3bcbdedb1c58ca98895a6945f8aa687d08
Anton Gusev <gans-AD@yandex.ru>
2022-09-30 07:36:20 +0300

    fix: валидация данных пользователя с  помощью Joi

--- routes/users.js
+++ routes/users.js
@@ -8,18 +8,6 @@ const {
   getCurrentUser,
 } = require('../controllers/users');
 
-const validationUser = () => celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    email: Joi.string().required().email(),
-    link: Joi.string()
-      .pattern(
-        /^(https?:\/\/)?(w{3}\.)?([a-z0-9.-]+)\.([a-z.]{2,6})([a-zA-Z0-9-._~:/?#[]@!$&'()*\+,;=]*)*#?/,
-      ),
-  }),
-});
-
 router.get('/', getUsers);
 router.get('/me', getCurrentUser);
 router.get(
@@ -31,7 +19,16 @@ router.get(
   }),
   userById,
 );
-router.patch('/me', validationUser, editUser);
+router.patch(
+  '/me',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+    }),
+  }),
+  editUser,
+);
 router.patch(
   '/me/avatar',
   celebrate({
